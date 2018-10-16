c98d561d84d817912da97df65561702c4d8a0f91
Marcus Edwards <marcusedwards@hotmail.ca>
2018-10-16 18:33:44 -0400

    build

---
+++ build/precache-manifest.d2952f728f80afa8af068669b431b58d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "068be206609d05c50c810ab3f86b63cc",
+    "url": "/static/media/qcircuit.068be206.png"
+  },
+  {
+    "revision": "e12e7bab6b26fcb7b2ba",
+    "url": "/static/css/main.aafe3d79.chunk.css"
+  },
+  {
+    "revision": "69d739f7d422e758a7b3a2437c4049b3",
+    "url": "/static/media/mobile-landing-background.69d739f7.png"
+  },
+  {
+    "revision": "c371007c42af5773feea",
+    "url": "/static/js/1.c371007c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "e12e7bab6b26fcb7b2ba",
+    "url": "/static/js/main.e12e7bab.chunk.js"
+  },
+  {
+    "revision": "abb933af64331ef1439704fc2929672c",
+    "url": "/static/media/ccircuit.abb933af.png"
+  },
+  {
+    "revision": "c1f136ee597bc5d0d05214f0d929425b",
+    "url": "/static/media/teleportationcircuit.c1f136ee.png"
+  },
+  {
+    "revision": "b7e26e53e8232d218261176a8e3d81a1",
+    "url": "/static/media/landing-background.b7e26e53.png"
+  },
+  {
+    "revision": "c371007c42af5773feea",
+    "url": "/static/css/1.6edcea85.chunk.css"
+  },
+  {
+    "revision": "b1ce7d74c4f75ac11e2db8d4eb56c8e1",
+    "url": "/index.html"
+  }
+];
